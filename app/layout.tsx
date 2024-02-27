9fca530b553c8d29f73d6324304eba30b137bd82
მაცი <93665709+maci03@users.noreply.github.com>
2024-02-28 00:35:12 +0400

    header part 1

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
 import { ThemeProvider } from "@mui/material";
 import theme from "@/styles/theme";
 import { COLORS } from "@/styles/colors";
+import Header from "@/components/shared/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,7 +23,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className} style={{backgroundColor: COLORS.lightGray, margin: 0}}>
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
-          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          <ThemeProvider theme={theme}><Header />{children}</ThemeProvider>
         </AppRouterCacheProvider>
       </body>
     </html>
---
+++ components/shared/header.tsx
@@ -0,0 +1,14 @@
+import { COLORS } from "@/styles/colors";
+import  Box from "@mui/material/Box";
+import React from "react";
+
+
+const Header = () =>{
+    return(
+        <Box sx={{backgroundColor: COLORS.darkBlue, color: COLORS.white}}>
+            Header
+        </Box>
+    );
+};
+
+export default Header
