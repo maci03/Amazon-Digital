bf6f4ca015c3121ebc10bb3d9e1fa3121ad8337d
მაცი <93665709+maci03@users.noreply.github.com>
2024-02-28 22:09:32 +0400

    working on search starter

---
+++ components/shared/SearchBar.tsx
@@ -0,0 +1,10 @@
+import { Paper } from "@mui/material";
+import React from "react";
+
+const SearchBar = () => {
+    return (
+        <Paper component="form" sx={{display: "flex", width: "80vh"}}>SearchBar</Paper>
+    )
+}
+
+export default SearchBar;
